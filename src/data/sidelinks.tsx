872d114a30eab42a0c03e5502e1e1ace98437114
RZCoding07 <muhammadrizkylubis@students.polmed.ac.id>
2025-02-17 01:19:24 +0700

    Remove unused side links from the navigation menu

--- src/data/sidelinks.tsx
+++ src/data/sidelinks.tsx
@@ -51,37 +51,6 @@ if (role === 'admin') {
       icon: <img width="20" height="20" src="https://img.icons8.com/stickers/20/organization.png" alt="organization" />
     },
     {
-      title: 'Dashboard',
-      label: '',
-      href: '/',
-      icon: <img width="20" height="20" src="https://img.icons8.com/stickers/20/virtual-machine2.png" alt="virtual-machine2" />,
-    },
-    {
-      title: 'Data Admin',
-      label: '',
-      href: '/admin',
-      icon: <img width="20" height="20" src="https://img.icons8.com/stickers/20/administrator-male.png" alt="administrator-male" />,
-    },
-    {
-      title: 'Data Employees',
-      label: '',
-      href: '/employee',
-      icon: <img width="20" height="20" src="https://img.icons8.com/stickers/20/permanent-job.png" alt="permanent-job" />,
-    },
-
-    {
-      title: 'Data Receptionist',
-      label: '',
-      href: '/receptionist',
-      icon: <img width="20" height="20" src="https://img.icons8.com/stickers/20/moleskine.png" alt="moleskine" />
-    },
-    {
-      title: 'Data Division',
-      label: '',
-      href: '/division',
-      icon: <img width="20" height="20" src="https://img.icons8.com/stickers/20/organization.png" alt="organization" />
-    },
-    {
       title: 'Scanner QRCode',
       label: '',
       href: '/scan-qr',
